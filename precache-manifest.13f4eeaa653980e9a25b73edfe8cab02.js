89e7b235bf66bdfd9ccee0dd611276d7b925a1a9
Abhikumar1511 <65035617+Abhikumar1511@users.noreply.github.com>
2020-05-09 14:26:05 +0530

    Updates

---
+++ precache-manifest.13f4eeaa653980e9a25b73edfe8cab02.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cec5561697d54687f9353cdb8d33ba3b",
+    "url": "/covid/index.html"
+  },
+  {
+    "revision": "329c1035d52103c44acc",
+    "url": "/covid/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "a7827338f8a463393fa7",
+    "url": "/covid/static/css/main.9c5a5020.chunk.css"
+  },
+  {
+    "revision": "329c1035d52103c44acc",
+    "url": "/covid/static/js/2.3bc0ba1f.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid/static/js/2.3bc0ba1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7827338f8a463393fa7",
+    "url": "/covid/static/js/main.4993fd1d.chunk.js"
+  },
+  {
+    "revision": "2efb2bde5ae38bc0020d",
+    "url": "/covid/static/js/runtime-main.d61a144f.js"
+  },
+  {
+    "revision": "7c15d7bb7deb262ae954248b78d40215",
+    "url": "/covid/static/media/capsule.7c15d7bb.jpg"
+  },
+  {
+    "revision": "19047b8126c31e2b8a2ff57a0a46462c",
+    "url": "/covid/static/media/doc.19047b81.jpg"
+  },
+  {
+    "revision": "7757a40a803f94f54cc937689e40c62b",
+    "url": "/covid/static/media/fvr.7757a40a.jpg"
+  },
+  {
+    "revision": "74fcc656d72584ae75d9fe0b8be24d81",
+    "url": "/covid/static/media/gocorona.74fcc656.jpg"
+  },
+  {
+    "revision": "c973e3565d85d3bfadf8f86bda3db521",
+    "url": "/covid/static/media/handw.c973e356.jpg"
+  },
+  {
+    "revision": "489eb170e0ddb83fcfb0fe103135a191",
+    "url": "/covid/static/media/heart.489eb170.jpg"
+  },
+  {
+    "revision": "1f01d0e14738f28132fcaebc504b73ca",
+    "url": "/covid/static/media/ind.1f01d0e1.png"
+  },
+  {
+    "revision": "7ae5ae896ceeddad75ac96f65749620e",
+    "url": "/covid/static/media/mask.7ae5ae89.jpg"
+  },
+  {
+    "revision": "247853a0a0f9c8eecd4687cc339240f5",
+    "url": "/covid/static/media/news1.247853a0.jpg"
+  },
+  {
+    "revision": "cc53a74b2bc0ee58d36026a25717408f",
+    "url": "/covid/static/media/sani.cc53a74b.jpg"
+  }
+]);
